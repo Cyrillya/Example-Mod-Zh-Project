f5d2398ffb6342030e4edf33dc708fa7cc979a82
lyc-Lacewing <lycrlsu01@gmail.com>
2022-09-27 18:13:14 +0800

    Translate ExampleDerivedPrefix

--- Content/Prefixes/ExampleDerivedPrefix.cs
+++ Content/Prefixes/ExampleDerivedPrefix.cs
@@ -1,14 +1,15 @@
 namespace ExampleMod.Content.Prefixes
 {
-	// Be sure to see 'ExamplePrefix' first.
-	// This class showcases how you can use inheritance to have an easier time with making variants of a single prefix.
-	// With that said, remember that inheritance is just one of the thousands of tools that are available to programmers, and that with great power comes great responsibility.
-	public class ExampleDerivedPrefix : ExamplePrefix // Deriving from ExamplePrefix!
+	// 确保你先看了“ExamplePrefix”
+	//这个类展示了你可以如何用继承来简单地制作一个前缀的变种
+	// 记住，继承只是上千中程序员的工具中的一种，能力越大，责任越大
+	public class ExampleDerivedPrefix : ExamplePrefix // 继承自 ExamplePrefix！
 	{
-		// Overriding the Power property to make it return 2.0 instead of 1.0!
-		// If you want to modify the base class' implementation's value, you can write 'base.Power' to reference it. 
+		// 重写 Power 属性使其返回 2.0 而不是 1.0！
+		// 如果你想要修改基类实现的值，你可以写“base.Power”来引用它
 		public override float Power => base.Power * 2f;
 
 		// No need for anything else, since members get inherited from the base class.
+		// 不需要其它东西，因为成员从基类继承过来了
 	}
 }
