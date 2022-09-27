7e69e40a3a92a33b635ae09f90810363b4fcb81b
Lacewing <103016663+lyc-Lacewing@users.noreply.github.com>
2022-09-27 15:22:32 +0800

    Update ExampleModRarity.cs

--- Content/Rarities/ExampleModRarity.cs
+++ Content/Rarities/ExampleModRarity.cs
@@ -5,14 +5,16 @@ namespace ExampleMod.Content.Rarities
 {
 	public class ExampleModRarity : ModRarity
 	{
+		// 该稀有度的颜色
 		public override Color RarityColor => new Color(200, 215, 230);
 
+		// 修饰词条可以影响稀有度
 		public override int GetPrefixedRarity(int offset, float valueMult) {
-			if (offset > 0) { // If the offset is 1 or 2 (a positive modifier).
-				return ModContent.RarityType<ExampleHigherTierModRarity>(); // Make the rarity of items that have this rarity with a positive modifier the higher tier one.
+			if (offset > 0) { // 如果修正值是1或2（正面的修饰词条）
+				return ModContent.RarityType<ExampleHigherTierModRarity>(); // 使物品的稀有度在有正面修饰词条时的变成更高的一个
 			}
 
-			return Type; // no 'lower' tier to go to, so return the type of this rarity.
+			return Type; // 没有更低级的稀有度去降了，所以就返回这个稀有度的Type
 		}
 	}
 }
