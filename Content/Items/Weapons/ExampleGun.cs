3aacffdb226942cf311c2745bdcd5708cd15fc49
lyc-Lacewing <lycrlsu01@gmail.com>
2022-09-27 08:49:28 +0800

    Add missed using Terraria.Localization

--- Content/Items/Weapons/ExampleGun.cs
+++ Content/Items/Weapons/ExampleGun.cs
@@ -6,6 +6,7 @@ using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 
 namespace ExampleMod.Content.Items.Weapons
 {
