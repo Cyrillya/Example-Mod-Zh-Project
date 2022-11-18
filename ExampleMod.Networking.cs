83650a29504b639c003fe28bc24d7f55841d9add
Cyrillya <59193481@qq.com>
2022-11-18 21:04:52 +0800

    修正与根目录完全汉化 (甚至icon也汉化了)

--- ExampleMod.Networking.cs
+++ ExampleMod.Networking.cs
@@ -5,35 +5,47 @@ using Terraria;
 
 namespace ExampleMod
 {
-	// This is a partial class, meaning some of its parts were split into other files. See ExampleMod.*.cs for other portions.
+	// 这是一个partial(分部)类，这意味着它的一些部分被拆分到其他文件中。 其他部分参见 ExampleMod.*.cs。
+	// 附: partial类微软文档: https://learn.microsoft.com/zh-cn/dotnet/csharp/programming-guide/classes-and-structs/partial-classes-and-methods
 	partial class ExampleMod
 	{
+		/// <summary>
+		/// 这是用来指定包类型的enum, 相当于给每个包分配一个指示身份的ID<br/>
+		/// <b>重要</b>: 建议搭配Visual Studio功能「查找所有引用」来看，因为这玩意不搭配发包看是看不懂的<br/>
+		/// 右键点击这个enum里面的某个类型，即可使用「查找所有引用」功能
+		/// </summary>
 		internal enum MessageType : byte
 		{
 			ExamplePlayerSyncPlayer,
 			ExampleTeleportToStatue
 		}
 
-		// Override this method to handle network packets sent for this mod.
-		//TODO: Introduce OOP packets into tML, to avoid this god-class level hardcode.
+		// 使用 HandlePacket 重写函数来控制由 ExampleMod 传入的网络数据包 (ModPacket)
+		// tML有把网络包进行重写改为「OOP架构」(即面向对象程序设计)的计划(不过只是计划罢了)
+		// 重写之后网络数据包会更容易调控，并且大大提升可读性
+		// tML的计划，有兴趣可以看看: https://github.com/tModLoader/tModLoader/issues/2436
+		// Cyril做的OOP收发包库，现在还没做ExampleMod(或者说示例就是更好的体验Mod): https://github.com/Cyrillya/NetSimplified-tModLoader
 		public override void HandlePacket(BinaryReader reader, int whoAmI) {
 			MessageType msgType = (MessageType)reader.ReadByte();
 
 			switch (msgType) {
-				// This message syncs ExamplePlayer.exampleLifeFruits
+				// 同步 ExamplePlayer.exampleLifeFruits
 				case MessageType.ExamplePlayerSyncPlayer:
 					byte playernumber = reader.ReadByte();
 					ExampleLifeFruitPlayer examplePlayer = Main.player[playernumber].GetModPlayer<ExampleLifeFruitPlayer>();
 					examplePlayer.exampleLifeFruits = reader.ReadInt32();
-					// SyncPlayer will be called automatically, so there is no need to forward this data to other clients.
+					// SyncPlayer将被自动调用，因此不需要将这些数据转发给其他客户端
+					// (这个包是在SyncPlayer进行发包的，详见ExampleLifeFruit.cs)
 					break;
 				case MessageType.ExampleTeleportToStatue:
+					// 根据读入的whoAmI标识，给对应NPC执行方法
 					if (Main.npc[reader.ReadByte()].ModNPC is ExamplePerson person && person.NPC.active) {
 						person.StatueTeleport();
 					}
 
 					break;
 				default:
+					// 这里的报错警告什么的都应该保持英文（你想用Language.GetTextValue也不是不行，但是得有英文）
 					Logger.WarnFormat("ExampleMod: Unknown Message type: {0}", msgType);
 					break;
 			}
--- ExampleMod.cs
+++ ExampleMod.cs
@@ -20,13 +20,14 @@ namespace ExampleMod
 		}
 
 		public override void Unload() {
-			// The Unload() methods can be used for unloading/disposing/clearing special objects, unsubscribing from events, or for undoing some of your mod's actions.
-			// Be sure to always write unloading code when there is a chance of some of your mod's objects being kept present inside the vanilla assembly.
-			// The most common reason for that to happen comes from using events, NOT counting On.* and IL.* code-injection namespaces.
-			// If you subscribe to an event - be sure to eventually unsubscribe from it.
+			// Unload() 方法可用于卸载(Unload)/处置(Dispose)/清除(Clear)特殊对象、取消事件委托或撤消你的模组的某些操作
+			// 当你的Mod的某些对象有可能保留在原版程序集中时，请务必编写卸载代码
+			// 发生这种情况的最常见原因来自使用事件委托，而不是 On.* 和 IL.* 代码注入命名空间 (On与IL命名空间委托在tML会被自动卸载，一般无需过多处理)
+			// 如果你进行了委托，请确保其被卸载
+			// 附: 委托相关微软文档: https://learn.microsoft.com/zh-cn/dotnet/csharp/programming-guide/delegates/
 
-			// NOTE: When writing unload code - be sure use 'defensive programming'. Or, in other words, you should always assume that everything in the mod you're unloading might've not even been initialized yet.
-			// NOTE: There is rarely a need to null-out values of static fields, since TML aims to completely dispose mod assemblies in-between mod reloads.
+			// 注意：在编写卸载代码时一定要使用“防御性编程”。也就是说，应该始终假设正在卸载的内容可能还没有被初始化 (可能没赋值就在运行Unload代码了)
+			// 注意：很少需要清空静态字段的值，因为 TML 旨在于 Mod 重新加载时完全处理 Mod 程序集。
 		}
 	}
 }
