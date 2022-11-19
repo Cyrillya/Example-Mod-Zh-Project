fbb45b1d80a1faf3bcbe76ea7c0d0c02a4016f9d
Cyrillya <59193481@qq.com>
2022-11-19 09:25:13 +0800

    Content/TileEntities

--- Content/TileEntities/AdvancedPylonTileEntity.cs
+++ Content/TileEntities/AdvancedPylonTileEntity.cs
@@ -8,43 +8,47 @@ using Terraria.ModLoader.Default;
 namespace ExampleMod.Content.TileEntities
 {
 	/// <summary>
-	/// This TileEntity is used in direct tandem with <seealso cref="ExamplePylonTileAdvanced"/> in order to grant more flexibility than
-	/// vanilla's normal pylon TileEntity (AKA <seealso cref="TETeleportationPylon"/>) using the <seealso cref="TEModdedPylon"></seealso> class
-	/// that is built into tML itself.
+	/// 此 TileEntity 与 <seealso cref="ExamplePylonTileAdvanced"/> 一同使用,
+	/// 以提供比原版普通晶塔 TileEntity（又称 <seealso cref="TETeleportationPylon"/>）更具灵活性的自定义晶塔<br/>
+	/// <seealso cref="TEModdedPylon"/> 类内置于 tML 本体中
 	/// <para>
-	/// The main example shown here is having a Pylon that is only active at completely random intervals.
+	/// 这里是一个仅在<b>完全随机的时间间隔</b>内处于活动状态的晶塔
 	/// </para>
 	/// </summary>
 	public class AdvancedPylonTileEntity : TEModdedPylon
 	{
-		// This is the main crux of this TileEntity; its pylon functionality will only work when this boolean is true.
+		/// <summary>
+		/// 这是这个 TileEntity 的关键，晶塔功能仅在它为 <see langword="true"/> 时才可用
+		/// </summary>
 		public bool isActive;
 
 		public override void OnNetPlace() {
-			// This hook is only ever called on the server; its purpose is to give more freedom in terms of syncing FROM the server to clients, which we take advantage of
-			// by making sure to sync whenever this hook is called:
+			// 这个方法只会在服务器端调用
+			// 需要在此处通过SendData方法发包，以同步TileEntity
 			NetMessage.SendData(MessageID.TileEntitySharing, number: ID, number2: Position.X, number3: Position.Y);
 		}
 
 		public override void NetSend(BinaryWriter writer) {
-			// We want to make sure that our data is synced properly across clients and server.
-			// NetSend is called whenever a TileEntitySharing message is sent, so the game will handle this automatically for us,
-			// granted that we send a message when we need to.
+			// 我们要确保数据在客户端和服务器之间正确同步
+			// 每当发送 TileEntitySharing 包时都会调用 NetSend，因此游戏会自动为我们处理这件事，前提是我们会在需要时发送数据
+			// 这里与下面的 NetReceive 相联系，以将 isActive 变量同步
 			writer.Write(isActive);
 		}
 
 		public override void NetReceive(BinaryReader reader) {
+			// 这里写自定义收包，请确保你发送了什么就要接收什么
+			// 与上面的 NetSend 相联系，以将 isActive 变量同步
 			isActive = reader.ReadBoolean();
 		}
 
 		public override void Update() {
-			// Update is only ever called on the Server or in SinglePlayer, so our randomness will be in that frame of reference
-			// Every tick, there will be a 1/180 chance that the active state of this pylon will swap (ON to OFF or vice versa)
+			// 更新仅在服务器端或单人游戏中调用，因此我们的随机性将在该参考系中
+			// 晶塔每帧都有 1/180 的几率切换其开关状态 (开启到关闭，反之亦然)
 			if (!Main.rand.NextBool(180)) {
 				return;
 			}
 
-			// Granted that the check passes, we change the active state, and if this is on the server, we sync it with the server:
+			// 在这里切换 isActive 的值，如果在服务器上别忘了同步数据
 			isActive = !isActive;
 			if (Main.netMode == NetmodeID.Server) {
 				NetMessage.SendData(MessageID.TileEntitySharing, number: ID, number2: Position.X, number3: Position.Y);
--- Content/TileEntities/SimplePylonTileEntity.cs
+++ Content/TileEntities/SimplePylonTileEntity.cs
@@ -3,9 +3,10 @@
 namespace ExampleMod.Content.TileEntities
 {
 	/// <summary>
-	/// This is an empty child class that acts exactly like the default implementation of the abstract <seealso cref="TEModdedPylon"/>
-	/// class, which itself acts nearly identical to vanilla pylon TEs. This inheritance only exists so that modded pylon entities
-	/// will properly have their "Mod" property set, for I/O purposes. Has the sealed modifier since this TE acts identical to its parent.
+	/// 这是一个继承 <seealso cref="TEModdedPylon"/> 的空子类，它与原版晶塔 TileEntity 完全相同<br/>
+	/// 这种继承可以让模组晶塔 TileEntity 正确地设置其“Mod”属性，以用于 I/O (输入/输出, 详情可百度) (这里指的是让tML那边可以正确操作这个晶塔)<br/>
+	/// 这个类由 <see langword="sealed"/>(封闭) 修饰，因为它和父类完全相同，没必要继承<br/>
+	/// Sealed参考: https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/keywords/sealed
 	/// </summary>
 	public sealed class SimplePylonTileEntity : TEModdedPylon { }
 }
