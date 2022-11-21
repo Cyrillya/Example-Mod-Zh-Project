a57e4569acc11174a5fd573c699bfb6502c359be
Cyrillya <59193481@qq.com>
2022-11-21 23:28:36 +0800

    K&R括号 + 一些细微修改

--- Content/Items/Weapons/ExampleSpecificAmmoGun.cs
+++ Content/Items/Weapons/ExampleSpecificAmmoGun.cs
@@ -1,7 +1,5 @@
-using ExampleMod.Content.Projectiles;
 using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -12,14 +10,14 @@ namespace ExampleMod.Content.Items.Weapons
 	public class ExampleSpecificAmmoGun : ModItem
 	{
 		public bool consumptionDamageBoost = false;
+
 		public override string Texture => "ExampleMod/Content/Items/Weapons/ExampleGun";
-		public override void SetStaticDefaults()
-		{
+
+		public override void SetStaticDefaults() {
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 		}
 
-		public override void SetDefaults()
-		{
+		public override void SetDefaults() {
 			// 普遍属性
 			Item.width = 62; // 物品的碰撞箱宽度(像素)
 			Item.height = 32; // 物品的碰撞箱高度(像素)
@@ -47,8 +45,7 @@ namespace ExampleMod.Content.Items.Weapons
 		}
 
 		// 这里写的是合成配方，合成配方在 Content/ExampleRecipes.cs 有更详尽的介绍
-		public override void AddRecipes()
-		{
+		public override void AddRecipes() {
 			CreateRecipe()
 				.AddIngredient<ExampleItem>()
 				.AddTile<Tiles.Furniture.ExampleWorkbench>()
@@ -56,68 +53,72 @@ namespace ExampleMod.Content.Items.Weapons
 		}
 
 		// 通过这个重写函数修改武器持握在玩家手上时的位置 (让他握着枪柄而不是反重力悬空)
-		public override Vector2? HoldoutOffset()
-		{
+		public override Vector2? HoldoutOffset() {
 			return new Vector2(2f, -2f);
 		}
 
-		public override void UpdateInventory(Player player)
-		{
-			// UpdateInventory 会在物品在玩家背包里调用
-			// 在这里我们要让这个bool变量设置为 false
+		public override void UpdateInventory(Player player) {
+			// 若物品处于玩家背包，就会调用 UpdateInventory
+			// 在这里我们要让这个 bool 变量设置为 false
 			// 具体这个变量有什么用，请继续往下看
 			consumptionDamageBoost = false;
 		}
 
-		public override bool? CanChooseAmmo(Item ammo, Player player)
-		{
+		public override bool? CanChooseAmmo(Item ammo, Player player) {
 			// CanChooseAmmo 可以让弹药被选择或者拒绝，而不受到弹药类型的限制
 			// (在弹药里有一个与它相同功能的重写函数 CanBeChosenAsAmmo)
 			// 在默认情况下，它会返回 null，只根据你在上面写的 useAmmo 来选择弹药
 			// 返回 true 将会强制使用该弹药，返回 false 将会强制无法使用该弹药
 			// 在本示例中，我们将会强制阻止诅咒弹的使用，但不影响其它子弹类型的弹药
-			if (ammo.type == ItemID.CursedBullet)
+			if (ammo.type == ItemID.CursedBullet) {
 				return false;
+			}
 
 			// 哦对了，我这边建议最好按照上面的说明，让它始终默认返回 null
 			// 你当然可以试着让它返回 true 或 false，不过这可能会产生一些难以预料的后果
 			return null;
 		}
 
-		public override bool CanConsumeAmmo(Item ammo, Player player)
-		{
-			// CanConsumeAmmo 可以控制弹药是否会消耗
-			// (在弹药里有一个与它相同功能的重写函数 CanBeConsumedAsAmmo)
+		public override bool CanConsumeAmmo(Item ammo, Player player) {
+			// CanConsumeAmmo 控制弹药是否被消耗
+			// (对于弹药类物品，有一个与它相同功能的重写函数 CanBeConsumedAsAmmo)
 			// 默认情况下会返回 true，当它返回 false 就会阻止弹药的消耗，不论为什么
-			// 注意，这并不代表返回 true 就会强制消耗弹药，像这样的功能需要IL或其它什么的
-			// 在本示例中，每轮发射的第一枪有20%的几率不消耗弹药
+			// 注意，这并不代表返回 true 就会强制消耗弹药，像这样的功能需要 IL 或 Detour
+
+			return player.ItemUsesThisAnimation switch {
+				// 每轮发射的第一枪有20%的几率不消耗弹药
+				0 => Main.rand.NextFloat() >= 0.20f,
+				// 而第二枪则有63%的几率不消耗弹药
+				1 => Main.rand.NextFloat() >= 0.63f,
+				// 第三枪则为36%的几率
+				2 => Main.rand.NextFloat() >= 0.36f,
+				_ => true
+			};
+
+			// 以上的代码使用 switch 以直观反映 ItemUsesThisAnimation 对应值所指向的结果
+			// 等同于以下代码 (此为官方 Example Mod 原代码)
+			/*
 			if (player.ItemUsesThisAnimation == 0)
 				return Main.rand.NextFloat() >= 0.20f;
-			// 而第二枪则有63%的几率不消耗弹药
 			else if (player.ItemUsesThisAnimation == 1)
 				return Main.rand.NextFloat() >= 0.63f;
-			// 第三枪则为36%的几率
 			else if (player.ItemUsesThisAnimation == 2)
 				return Main.rand.NextFloat() >= 0.36f;
-
-			return true;
+			 */
 		}
 
-		public override void OnConsumeAmmo(Item ammo, Player player)
-		{
+		public override void OnConsumeAmmo(Item ammo, Player player) {
 			// OnConsumeAmmo 将会在弹药被消耗时调用到
-			// (在弹药里有一个与它相同功能的重写函数 OnConsumedAsAmmo)
+			// (对于弹药类物品，有一个与它相同功能的重写函数 OnConsumedAsAmmo)
 			// 在这里，我们把一个负责控制伤害加成的bool变量设为 true
 			// 这会让这把枪在消耗弹药后的下一次射击获得伤害加成
 			consumptionDamageBoost = true;
 		}
 
-		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
-		{
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
 			// 如果该变量为 true 则将本次伤害提高
 			// 提问：想一想，为什么不需要重置伤害？
-			if (consumptionDamageBoost)
-			{
+			if (consumptionDamageBoost) {
 				damage = (int)(damage * 1.2f);
 			}
 		}
