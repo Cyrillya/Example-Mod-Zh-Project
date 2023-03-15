3caf20dc490462baf90fd24c70b2f156f2ff631b
GlassCabbage <1421498250@qq.com>
2023-03-15 17:15:39 +0800

    ExampleCactus.cs 翻译

--- Content/Tiles/Plants/ExampleCactus.cs
+++ Content/Tiles/Plants/ExampleCactus.cs
@@ -4,18 +4,21 @@ using Terraria.ModLoader;
 
 namespace ExampleMod.Content.Tiles
 {
+	// ModCactus这个类允许你自定义自己的仙人掌类植物
 	public class ExampleCactus : ModCactus
 	{
 		public override void SetStaticDefaults() {
-			// Makes Example Cactus grow on ExampleOre
+			// 使示例仙人掌在示例矿石上生长
 			GrowsOnTileId = new int[1] { ModContent.TileType<ExampleOre>() };
 		}
 
 		public override Asset<Texture2D> GetTexture() {
+			// 获得示例仙人掌的纹理
 			return ModContent.Request<Texture2D>("ExampleMod/Content/Tiles/Plants/ExampleCactus");
 		}
 
-		// This would be where the Cactus Fruit Texture would go, if we had one.
+		// 如果你的仙人掌会结果实，你可以用上面GetTexture里相同的方法获得果实纹理
+		// 这个例子里我们不让他长果实，直接返回null
 		public override Asset<Texture2D> GetFruitTexture() {
 			return null;
 		}
